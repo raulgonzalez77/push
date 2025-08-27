80b1433be61ec832274dd1ca61376ee792c0fb72
Raul Gonzalez <54822905+raulgonzalez77@users.noreply.github.com>
2025-08-27 12:45:21 -0500

    updates

--- sw.js
+++ sw.js
@@ -2,10 +2,13 @@
 self.addEventListener('push', function(event) {
     console.log('Received a push message', event);
 
+// raul added the skipWaiting event
+self.skipWaiting();
+
     var title = 'Registration is now open';
     var body = 'We have received a push message.';
     var icon = 'https://www.blinn.edu/_files/images/ico/favicon.ico';
-    var tag = 'tag-8';
+    var tag = 'tag-9';
     var data = {
     doge: {
         wow: 'such amaze notification data'
