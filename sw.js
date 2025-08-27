015e33741160e4ead6892a0a0c6e0127a3197b9b
Raul Gonzalez <speedygonzales77@gmail.com>
2025-08-27 12:34:48 -0500

    updates

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@
 self.addEventListener('push', function(event) {
     console.log('Received a push message', event);
 
-    var title = 'Yay a message.';
+    var title = 'Registration is now open';
     var body = 'We have received a push message.';
-    var icon = '/images/icon-192x192.png';
-    var tag = 'simple-push-demo-notification-tag-6';
+    var icon = 'https://www.blinn.edu/_files/images/ico/favicon.ico';
+    var tag = 'simple-push-demo-notification-tag-7';
     var data = {
     doge: {
         wow: 'such amaze notification data'
