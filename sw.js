365fcfb098ed2afc05f03599b2a92c24d74e892c
Raul Gonzalez <54822905+raulgonzalez77@users.noreply.github.com>
2025-11-11 13:49:13 -0600

    updaes

--- sw.js
+++ sw.js
@@ -1,31 +1,19 @@
-
 self.addEventListener('push', function(event) {
     console.log('Received a push message', event);
 
-//self.addEventListener('install', (event) => {
+    const data = event.data ? event.data.json() : {};
+    const title = data.title || 'Registration is now open';
+    const body = data.body || 'We have received a push message.';
+    const icon = data.icon || 'https://www.blinn.edu/_files/images/ico/favicon.ico';
+    const tag = data.tag || 'tag-25';
+    const extraData = data.extra || {};
 
-    var title = 'Registration is now open';
-    var body = 'We have received a push message.';
-    var icon = 'https://www.blinn.edu/_files/images/ico/favicon.ico';
-    var tag = 'tag-24';
-    var data = {
-    doge: {
-        wow: 'such amaze notification data'
-    }
-    };
-
-    // raul added the skipWaiting event
-    self.skipWaiting();
     event.waitUntil(
-    self.registration.showNotification(title, {
-        body: body,
-        icon: icon,
-        tag: tag,
-        data: data
-    })
+        self.registration.showNotification(title, {
+            body: body,
+            icon: icon,
+            tag: tag,
+            data: extraData
+        })
     );
-
-//}); //install
-
-
 });
